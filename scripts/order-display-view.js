eca64dacb749b4d2f8679be42d79f33934de0a86
Dan Hart <danhart@notonthehighstreet.com>
2014-08-19 18:43:36 +0100

    Basic orders view implementation

--- scripts/order-display-view.js
+++ scripts/order-display-view.js
@@ -3,12 +3,22 @@ define(['backbone', 'mustache', 'jquery'], function(Backbone, mustache, $) {
 
     var OrderDisplayView = Backbone.View.extend({
         initialize: function() {
+            this.$el = $(this.el);
             this.listenTo( this.collection, 'add', this.render );
+            this.xPosition = window.innerWidth;
+            this.setXPosition();
         },
 
         render: function(order) {
-            var html = mustache.render(template, order.attributes);
-            $('#order_display').append($(html));
+            var $order = $(mustache.render(template, order.attributes));
+            $order.width(window.innerWidth);
+            this.$el.append($order);
+            this.xPosition -= $order.width();
+            this.setXPosition();
+        },
+
+        setXPosition: function() {
+            this.$el.css('-webkit-transform', 'translate3d(' + this.xPosition + 'px,0,0)');
         }
     });
 
